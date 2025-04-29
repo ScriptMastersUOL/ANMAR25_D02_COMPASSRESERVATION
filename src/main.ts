b0c19d458aea6e002a13f4da80a361cc6214c95f
Gabryel <gabryel.macedo@gmail.com>
2025-04-29 14:50:32 -0300

    feat: configure swagger for automatic documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Compass Reservation API')
+    .setDescription('API documentation for the compass reservation system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document)
+    
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
