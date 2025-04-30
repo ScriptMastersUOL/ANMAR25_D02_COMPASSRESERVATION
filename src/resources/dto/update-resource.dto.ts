60cbc896b57c5b76caee1cf257ba37e998874fdc
Gabryel Souza <gabryel.macedo13@gmail.com>
2025-04-30 15:57:27 -0300

    Revert "Feat/create resources dtos"

--- src/resources/dto/update-resource.dto.ts
+++ src/resources/dto/update-resource.dto.ts
@@ -1,11 +1,4 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateResourceDto } from './create-resource.dto';
 
-export class UpdateResourceDto extends PartialType(CreateResourceDto) {
-    @ApiPropertyOptional({ description: 'New name of the resource' })
-  name?: string;
-
-  @ApiPropertyOptional({ example: 'Updated description for the resource' })
-  description?: string;
-}
+export class UpdateResourceDto extends PartialType(CreateResourceDto) {}
