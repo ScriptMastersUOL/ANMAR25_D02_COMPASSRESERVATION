eb512b259d77aeba51f04f5e0937919f80c1945c
Gabryel <gabryel.macedo@gmail.com>
2025-04-30 15:17:08 -0300

    feat: create resources DTOs

--- src/resources/dto/create-resource.dto.ts
+++ src/resources/dto/create-resource.dto.ts
@@ -1,3 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsInt, IsNotEmpty, IsString, Min } from "class-validator";
 
-export class CreateResourceDto {}
+export class CreateResourceDto {
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @ApiProperty()
+    @IsInt()
+    @Min(1)
+    quantity: number;
+    
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+}
