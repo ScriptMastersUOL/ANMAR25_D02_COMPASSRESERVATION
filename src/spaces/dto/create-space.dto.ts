47cb3d4bc5381ef58297cb8589b97d2b2640a223
Gabryel Souza <gabryel.macedo13@gmail.com>
2025-04-30 16:09:08 -0300

    Revert "Feat/create resources dtos"

--- src/spaces/dto/create-space.dto.ts
+++ src/spaces/dto/create-space.dto.ts
@@ -1,17 +1,1 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
-import { isActive } from '../../enums/isActive.enum';
-
-export class CreateSpaceDto {
-  @IsNotEmpty()
-  name: string;
-
-  @IsNotEmpty()
-  description: string;
-
-  @IsNotEmpty()
-  capacity: string;
-
-  @IsNotEmpty()
-  @IsEnum(isActive)
-  isActive: number;
-}
+export class CreateSpaceDto {}
