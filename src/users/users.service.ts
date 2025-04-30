2203681bad92f1342849bd03c157b0f047755349
Felipe <felipe.martini2404@gmail.com>
2025-04-30 14:59:26 -0300

    chore: register prismaService in app.module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
