abf99264424e8319f2c996c6a13eee459d55d9c3
Gabryel <gabryel.macedo@gmail.com>
2025-04-30 15:44:54 -0300

    feat: add example and description to ApiProperty for swagger documentation

--- src/resources/dto/create-resource.dto.ts
+++ src/resources/dto/create-resource.dto.ts
@@ -3,17 +3,17 @@ import { IsInt, IsNotEmpty, IsString, Min } from "class-validator";
 
 export class CreateResourceDto {
 
-    @ApiProperty()
+    @ApiProperty({ example: 'Projector', description: 'The name of the resource (must be unique)' })
     @IsNotEmpty()
     @IsString()
     name: string;
 
-    @ApiProperty()
+    @ApiProperty({ example: 10, description: 'Available quantity of the resource' })
     @IsInt()
     @Min(1)
     quantity: number;
     
-    @ApiProperty()
+    @ApiProperty({description: 'Description of the resource' })
     @IsNotEmpty()
     @IsString()
     description: string;
--- src/resources/dto/update-resource.dto.ts
+++ src/resources/dto/update-resource.dto.ts
@@ -1,4 +1,11 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateResourceDto } from './create-resource.dto';
 
-export class UpdateResourceDto extends PartialType(CreateResourceDto) {}
+export class UpdateResourceDto extends PartialType(CreateResourceDto) {
+    @ApiPropertyOptional({ description: 'New name of the resource' })
+  name?: string;
+
+  @ApiPropertyOptional({ example: 'Updated description for the resource' })
+  description?: string;
+}
