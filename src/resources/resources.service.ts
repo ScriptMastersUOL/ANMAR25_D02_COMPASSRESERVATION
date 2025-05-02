d4a4f6e85e8da75d9de71a55d34ba1137426b557
Gabryel <gabryel.macedo@gmail.com>
2025-05-02 19:22:34 -0300

    feat(resources): implementing findOne  method to list resources by id

--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -1,46 +1,60 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateResourceDto } from './dto/create-resource.dto';
 import { UpdateResourceDto } from './dto/update-resource.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class ResourcesService {
-
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createResourceDto: CreateResourceDto) {
     const { name } = createResourceDto;
 
-    const nameExists = await this.prismaService.resource.findUnique({where: { name }})
+    const nameExists = await this.prismaService.resource.findUnique({
+      where: { name },
+    });
     if (nameExists) {
       throw new BadRequestException('Resource name already registered');
     }
 
     return this.prismaService.resource.create({
-      data: createResourceDto
-    })
+      data: createResourceDto,
+    });
   }
 
   findAll() {
     return `This action returns all resources`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} resource`;
+  async findOne(id: number) {
+    const resource = await this.prismaService.resource.findUnique({ where: { id } });
+
+    if (!resource) {
+      throw new NotFoundException('Resource not found');
+    }
+
+    return resource;
   }
 
   async update(id: number, updateResourceDto: UpdateResourceDto) {
-    const resource = await this.prismaService.resource.findUnique({where: { id }})
+    const resource = await this.prismaService.resource.findUnique({
+      where: { id },
+    });
 
-    if(!resource) {
-      throw new BadRequestException('Resource not found')
+    if (!resource) {
+      throw new NotFoundException('Resource not found');
     }
 
     if (updateResourceDto.name) {
       const nameExists = await this.prismaService.resource.findUnique({
         where: { name: updateResourceDto.name },
       });
-  
+
       if (nameExists && nameExists.id !== id) {
         throw new BadRequestException('The resource name already exists');
       }
