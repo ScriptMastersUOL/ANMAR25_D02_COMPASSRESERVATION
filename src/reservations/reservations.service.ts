20aa16cf3ecc77b7c7965f8bc85906388b3e1028
brenocidade <brenocidade01@gmail.com>
2025-05-04 12:03:53 -0300

    feat: implement reservation creation with validation and resource management

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,26 +1,123 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SpacesService } from 'src/spaces/spaces.service';
+import { ResourcesService } from 'src/resources/resources.service';
+import { ClientsService } from 'src/clients/clients.service';
 
 @Injectable()
 export class ReservationsService {
-  create(createReservationDto: CreateReservationDto) {
-    return 'This action adds a new reservation';
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly spacesService: SpacesService,
+    private readonly resourcesService: ResourcesService,
+    private readonly clientsService: ClientsService,
+  ) { }
+
+  async create(createReservationDto: CreateReservationDto) {
+    try {
+      const space = await this.spacesService.findOne(createReservationDto.spaceId)
+
+      const resource = await this.resourcesService.findOne(createReservationDto.resourceId)
+
+      const client = await this.clientsService.findOne(createReservationDto.clientId)
+
+      const overlappingReservation = await this.prismaService.reservation.findFirst({
+        where: {
+          spaceId: createReservationDto.spaceId,
+          AND: [
+            {
+              startDate: {
+                lt: new Date(createReservationDto.endDate)
+              }
+            },
+            {
+              endDate: {
+                gt: new Date(createReservationDto.startDate)
+              }
+            }
+          ]
+        }
+      });
+
+      if (createReservationDto.startDate >= createReservationDto.endDate) {
+        throw new BadRequestException('Start date must be before end date');
+      }
+
+      if (space.isActive === 0) {
+        throw new BadRequestException('Space is not active');
+      }
+
+      if (resource.isActive === 0) {
+        throw new BadRequestException('Resource is not active');
+      }
+
+      if (client.isActive === 0) {
+        throw new BadRequestException('Client is not active');
+      }
+
+      if (resource.quantity < 0) {
+        throw new BadRequestException('Resource is not available');
+      }
+
+      if (overlappingReservation) {
+        throw new BadRequestException('Reservation overlaps with another space');
+      }
+
+      if (resource.quantity) {
+        resource.quantity = resource.quantity - 1;
+        await this.resourcesService.update(resource.id, {
+          ...resource,
+          quantity: resource.quantity
+        })
+      }
+
+      return this.prismaService.reservation.create({
+        data: {
+          ...createReservationDto,
+          startDate: new Date(createReservationDto.startDate),
+          endDate: new Date(createReservationDto.endDate),
+          reservationResources: {
+            create: {
+              resourceId: createReservationDto.resourceId,
+            }
+          }
+        },
+        include: {
+          client: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          space: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          reservationResources: {
+            include: {
+              resource: {
+                select: {
+                  id: true,
+                  name: true,
+                },
+              },
+            },
+          },
+        }
+      })
+
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      } else {
+        throw new BadRequestException('Error creating reservation');
+      }
+    }
   }
 
-  findAll() {
-    return `This action returns all reservations`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} reservation`;
-  }
-
-  update(id: number, updateReservationDto: UpdateReservationDto) {
-    return `This action updates a #${id} reservation`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} reservation`;
-  }
 }
