cb22dbbcc9a287463132bfb96c9d30bdea0bebdf
Gabryel <gabryel.macedo@gmail.com>
2025-05-06 13:03:16 -0300

    feat(client): add swagger decorator to client dtos

--- src/clients/dto/find-clients-query.dto.ts
+++ src/clients/dto/find-clients-query.dto.ts
@@ -2,32 +2,39 @@
 import { IsOptional, IsString, IsIn, IsInt, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { isActive } from 'src/enums/isActive.enum';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class FindClientsQueryDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   email?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   cpf?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
   @IsIn([isActive.active, isActive.disabled])
   status?: isActive;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
   @Min(1)
   page: number = 1;
-
+  
+  @ApiPropertyOptional()
   @IsOptional()
   @Transform(({ value }) => parseInt(value, 10))
   @IsInt()
