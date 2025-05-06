a1a9110e142a4fdbca0bd28364dada7e50f6a560
Gabryel <gabryel.macedo@gmail.com>
2025-05-06 13:04:26 -0300

    feat(spaces): add swagger decorator to spaces dtos

--- src/spaces/dto/update-space.dto.ts
+++ src/spaces/dto/update-space.dto.ts
@@ -1,16 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSpaceDto } from './create-space.dto';
 import { IsOptional, IsString, IsInt, Min } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateSpaceDto extends PartialType(CreateSpaceDto) {
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   name?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsInt()
   @Min(1, { message: 'Capacity must be greater or equal to 1' })
